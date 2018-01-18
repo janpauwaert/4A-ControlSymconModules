30ec68381957c4595fc5909417059ca751e07854
4a-control <35578223+janpauwaert@users.noreply.github.com>
2018-01-18 23:25:24 +0100

    module
    profielen aangepast

--- S7DigitalInput/module.php
+++ S7DigitalInput/module.php
@@ -10,19 +10,58 @@ class S7DigitalInput extends IPSModule
 		$this->RegisterPropertyInteger('sensorInstanceId', 0);
 
 		// Create variable profiles
-		if (@IPS_GetVariableProfile('DigitalInput') == false)
+		if (@IPS_VariableProfileExits('xAan') == false)
 		{
-			IPS_CreateVariableProfile('DigitalInput', 1);
-			IPS_SetVariableProfileIcon('DigitalInput', 'Flag');
+			IPS_CreateVariableProfile('xAan', 0);
+			IPS_SetVariableProfileIcon('xAan', 'Flag');
+			IPS_SetVariableProfileAssociation("xAan",false,"Uit");
+			IPS_SetVariableProfileAssociation("xAan",true,"Aan",0x00ff00);
+		}
+
+		if (@IPS_VariableProfileExits('xUit') == false)
+		{
+			IPS_CreateVariableProfile('xUit', 0);
+			IPS_SetVariableProfileIcon('xUit', 'Flag');
+			IPS_SetVariableProfileAssociation("xUit",false,"Uit");
+			IPS_SetVariableProfileAssociation("xUit",true,"Aan",0x00ff00);
 
 		}
 
+		if (@IPS_VariableProfileExits('xMode') == false)
+		{
+			IPS_CreateVariableProfile('xUit', 0);
+			IPS_SetVariableProfileIcon('xUit', 'Gear');
+			IPS_SetVariableProfileAssociation("xUit",false,"Automatisch");
+			IPS_SetVariableProfileAssociation("xUit",true,"Manueel");
+		}
+
+		if (@IPS_VariableProfileExits('xOnbAlarm') == false)
+		{
+			IPS_CreateVariableProfile('xOnbAlarm', 0);
+			IPS_SetVariableProfileIcon('xOnbAlarm', 'Warning');
+			IPS_SetVariableProfileAssociation("xOnbAlarm",false,"");
+			IPS_SetVariableProfileAssociation("xOnbAlarm",true,"",0xff0000);
+		}
+
+		if (@IPS_VariableProfileExits('xAlarm') == false)
+		{
+			IPS_CreateVariableProfile('xAlarm', 0);
+			IPS_SetVariableProfileIcon('xAlarm', 'Warning');
+			IPS_SetVariableProfileAssociation("xAlarm",false,"");
+			IPS_SetVariableProfileAssociation("xAlarm",true,"",0xff0000);
+		}
+
+
+
+
+
+
 		// Create status variables
-		$this->registerVariableFloat('xAan', 'Input staat aan', 'Aan', 0);
-		$this->registerVariableFloat('xUit', 'Input staat Uit', 'Uit', 1);
-		$this->registerVariableFloat('xMode', 'Manuele mode als 1', 'Mode', 2);
-		$this->registerVariableFloat('xOnbAlarm', 'Er zijn onbevestigde alarmen', 'Onbevestig alarm', 3);
-		$this->registerVariableFloat('xAlarm', 'Er is een Alarm', 'Alarm', 4);
+		$this->registerVariableBoolean('xAan', 'Input staat aan', 'Aan', 0);
+		$this->registerVariableBoolean('xUit', 'Input staat Uit', 'Uit', 1);
+		$this->registerVariableBoolean('xMode', 'Manuele mode als 1', 'Mode', 2);
+		$this->registerVariableBoolean('xOnbAlarm', 'Er zijn onbevestigde alarmen', 'Onbevestig alarm', 3);
+		$this->registerVariableBoolean('xAlarm', 'Er is een Alarm', 'Alarm', 4);
 
 
 		// Create event
@@ -42,14 +81,6 @@ class S7DigitalInput extends IPSModule
 		// Never delete this line
 		parent::ApplyChanges();
 
-		if ($sensorId = $this->ReadPropertyInteger('sensorInstanceId'))
-		{
-			// Validate if compatible instance id was selected and set update event
-			if ($this->ProcessValues() == true)
-			{
-				$this->setUpdateEvent();
-			}
-		}
 	}
 
 	
