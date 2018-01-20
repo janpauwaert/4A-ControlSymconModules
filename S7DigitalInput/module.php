7916f6180a08815997dea35581455159f14d2e27
4a-control <35578223+janpauwaert@users.noreply.github.com>
2018-01-20 11:47:56 +0100

    d

--- S7DigitalInput/module.php
+++ S7DigitalInput/module.php
@@ -57,9 +57,9 @@ class S7DigitalInput extends IPSModule
 
 		$InsID = IPS_CreateInstance ( "{932076B1-B18E-4AB6-AB6D-275ED30B62DB}" ) ;
 
-		IPS_SetName ( $InsID , "S7_Input" .  IPS_GetProperty ( $this , "Id" ));  // noem de instantie
+		IPS_SetName ( $InsID , "S7_Input" .  $this -> ReadPropertyInteger ( "Id" ));  // noem de instantie
  		IPS_SetParent ( $InsID , 0 ) ;  // sorteer instantie onder object met objectID "12345"
- 		$config = sprintf('{"DataType":1,"Area":7,"AreaAddress":1000,"Address":%s,"Bit":0,"Length":0,"Poller":100,"ReadOnly":false,"EmulateStatus":true,"Factor":0.0}', IPS_GetProperty ( 40585 , "Id" )*2);
+ 		$config = sprintf('{"DataType":1,"Area":7,"AreaAddress":1000,"Address":%s,"Bit":0,"Length":0,"Poller":100,"ReadOnly":false,"EmulateStatus":true,"Factor":0.0}', $this -> ReadPropertyInteger ( "Id" )*2);
 		IPS_SetConfiguration ( $InsID , $config) ;
 		IPS_ApplyChanges ( $InsID ) ;  // accepteer nieuwe configuratie 
 
