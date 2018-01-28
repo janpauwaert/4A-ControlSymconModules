398ebe1d32e254475685de7799c153df2538767f
4a-control <35578223+janpauwaert@users.noreply.github.com>
2018-01-28 12:00:56 +0100

    d

--- S7Object/module.php
+++ S7Object/module.php
@@ -400,21 +400,10 @@ class S7Object extends IPSModule
 			   	}
 
 			    break;
-			switch ($Interface){
-			    	case 0:
-			   			$poller = 100;
-			   			break;
-			   		case 1:
-			   			$poller = 0;
-			   			break;
-			   		case 2:
-			   			$poller = 100;
-			   			break;
-			   	}
 		}
 
 			//IPS_SetName ( $InsID , sprintf("S7_PLC_Connection_%s_%s"),$InputType,$this->ReadPropertyInteger("Id"));  // noem de instantie volgens het type en nr
-			$config = sprintf('{"DataType":%s,"Area":7,"AreaAddress":%s,"Address":%s,"Bit":0,"Length":1,"Poller":%s,"ReadOnly":false,"EmulateStatus":true,"Factor":0.0}',$DataType,$AreaAddress, $Address,$poller);
+			$config = sprintf('{"DataType":%s,"Area":7,"AreaAddress":%s,"Address":%s,"Bit":0,"Length":1,"Poller":0,"ReadOnly":false,"EmulateStatus":true,"Factor":0.0}',$DataType,$AreaAddress, $Address);
 			IPS_SetConfiguration ( $OBJid , $config) ;
 			IPS_ApplyChanges ( $OBJid ) ;  // accepteer nieuwe configuratie 
 
