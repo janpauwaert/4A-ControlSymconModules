8d2f34f547a9403e824e12fd2d58351624538de2
jan pauwaert <jan@pauwaert.nl>
2019-11-28 12:06:56 +0100

    float

--- Zone/module.php
+++ Zone/module.php
@@ -290,7 +290,8 @@
 			$v= hexdec($strHex);
 			$x = ($v & ((1<<23)-1))+(1<<23)*($v>>31|1);
 			$exp = ($v >> 23 & 0xFF) - 127;
-			return $x * pow(2,$exp-23);
+			$float = $x * pow(2,$exp-23);
+			return $float;
 		}
 
 
